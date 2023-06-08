85caad2d6d97d5a20b1a8b2591ec3905e4fac09e
devkalelbranco <dev.kalel.branco@gmail.com>
2023-06-08 12:10:44 -0300

    Feat: save and remove employee

--- src/main/java/br/com/kalel/organoserver/controller/EmployeeController.java
+++ src/main/java/br/com/kalel/organoserver/controller/EmployeeController.java
@@ -1,26 +1,26 @@
 package br.com.kalel.organoserver.controller;
 
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.kalel.organoserver.models.Employee;
-import br.com.kalel.organoserver.models.Team;
 import br.com.kalel.organoserver.repository.EmployeeRepository;
-import br.com.kalel.organoserver.repository.TeamRepository;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
     
     private final EmployeeRepository repository;
-    private final TeamRepository teamRepository;
 
-    public EmployeeController(EmployeeRepository repository, TeamRepository teamRepository){
+    public EmployeeController(EmployeeRepository repository){
         this.repository = repository;
-        this.teamRepository = teamRepository;
     }
 
     @GetMapping("")
@@ -28,9 +28,14 @@ public class EmployeeController {
         return this.repository.findAll();
     }
     
-    @PostMapping()
+    @PostMapping("")
     public Employee storeEmployee(@RequestBody Employee employee){
         return this.repository.save(employee);
+    }
+
+    @DeleteMapping("")
+    public void removeEmployee(@PathVariable UUID id){
+        this.repository.delete(this.repository.findById(id).get());
     }
 
 }
--- src/main/java/br/com/kalel/organoserver/repository/EmployeeRepository.java
+++ src/main/java/br/com/kalel/organoserver/repository/EmployeeRepository.java
@@ -1,10 +1,12 @@
 package br.com.kalel.organoserver.repository;
 
 
+import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.kalel.organoserver.models.Employee;
 
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, UUID> {
 
 }
